2ede94b52956dc22fdd3db5d3ae53ca5fdb09f8f
Stephen Wong <4060163+yellowcandle@users.noreply.github.com>
2024-12-30 03:17:11 +0800

    Your commit message here

--- face_detect.py
+++ face_detect.py
@@ -3,7 +3,11 @@ import cv2
 import numpy as np
 import pandas as pd
 import tqdm
+import warnings
 from insightface.app import FaceAnalysis
+
+# Suppress the specific FutureWarning from numpy.linalg.lstsq
+warnings.filterwarnings('ignore', category=FutureWarning, module='insightface.utils.transform')
 
 # Get the absolute path of the current script
 current_script_path = os.path.abspath(__file__)
